b7fe3f7ebda847e58f6b05ae537b92f1707a5b89
lakshmikanth <kanth.explorer@gmail.com>
2018-12-14 01:44:51 +0800

    JS tests for reservation and transfer positive and negative usecase assertion

--- test/DNSDappMasterTest.js
+++ test/DNSDappMasterTest.js
@@ -11,6 +11,19 @@ contract('DNSDappMaster', function (accounts) {
     })
   })
 
+  it('assert that system can Reserve a new DNSName', function () {
+    let nameOwner = accounts[1]
+    return DNSDappMaster.deployed().then(function (instance) {
+      return instance.reserveDNSName('lakshmikanth.p', { from: nameOwner, value: web3.toWei(1, 'ether') })
+        .then(function (result) {
+          assert.isOk(result.receipt)
+          return instance.isAValidDNSNameString.call('lakshmikanth.p').then(function (reservationEvent) {
+            assert.equal(reservationEvent, true, 'lakshmikanth.p is now reserved')
+          })
+        })
+    })
+  })
+
   it('assert that system can Reserve a new DNSName and Transfer funds by using DNSName', function () {
     let nameOwner = accounts[1]
     let etherSender = accounts[2]
@@ -23,9 +36,29 @@ contract('DNSDappMaster', function (accounts) {
           })
         })
         .then(function () {
-          return instance.transferEtherByDNSName('lakshmikanth', { from: etherSender, value: web3.toWei(2, 'ether') })
+          return instance.transferEtherByDNSName('lakshmikanth', { from: etherSender, value: web3.toWei(0.1, 'ether') })
             .then(function (result) {
               assert.isOk(result.receipt)
+            })
+        })
+    })
+  })
+
+  it('assert that system cannot Transfer funds to non-existing DNSName', function () {
+    let nameOwner = accounts[1]
+    let etherSender = accounts[2]
+    return DNSDappMaster.deployed().then(function (instance) {
+      return instance.reserveDNSName('lakshmi.kanth', { from: nameOwner, value: web3.toWei(1, 'ether') })
+        .then(function (result) {
+          assert.isOk(result.receipt)
+          return instance.isAValidDNSNameString.call('lakshmi.kanth').then(function (reservationEvent) {
+            assert.equal(reservationEvent, true, 'lakshmi.kanth is now reserved')
+          })
+        })
+        .then(function () {
+          return instance.transferEtherByDNSName('lakshmikanth.pk', { from: etherSender, value: web3.toWei(2, 'ether') })
+            .then(function (result) {
+              assert.fail(result.receipt)
             })
         })
     })
